d655924a762d8796fea29b0d18e105a06fedff18
Antonio Barandica <anjose.121@gmail.com>
2019-08-28 18:01:53 -0500

    Add env var and local postgres

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra/contrib'
 require 'sinatra/namespace'
 
 # DB = Sequel.connect(adapter: :postgres, database: 'my_db', host: 'localhost', user: 'db_user')
-DB = Sequel.sqlite('db/development.sqlite3')
+DB = Sequel.postgres('test', user: 'antonio', password: 'admin', host: 'localhost')
 
 %w[models routes].each { |dir| Dir.glob("./#{dir}/*.rb", &method(:require)) }
 
