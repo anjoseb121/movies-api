f2747ae6c1fe85b885f73c62f3ce8f30f60c2f41
Antonio Barandica <anjose.121@gmail.com>
2019-08-28 18:48:49 -0500

    Use tmp folder for heroku

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sequel'
 require 'sinatra/contrib'
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
+#DB = Sequel.postgres('test', user: 'antonio', password: 'admin', host: 'localhost')
 
 %w[models routes].each { |dir| Dir.glob("./#{dir}/*.rb", &method(:require)) }
 
