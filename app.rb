d65d8f092b94a90777782914d91e4affc594a81f
Antonio Barandica <anjose.121@gmail.com>
2019-08-28 18:14:12 -0500

    Add rakefile

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require 'sequel'
 require 'sinatra/contrib'
 require 'sinatra/namespace'
 
-# DB = Sequel.connect(adapter: :postgres, database: 'my_db', host: 'localhost', user: 'db_user')
 DB = Sequel.postgres('test', user: 'antonio', password: 'admin', host: 'localhost')
 
 %w[models routes].each { |dir| Dir.glob("./#{dir}/*.rb", &method(:require)) }
